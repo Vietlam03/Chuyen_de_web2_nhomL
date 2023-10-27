3f9428b96ed20cac4000acf2d7804607e8ae2d93
Vietlam03 <21211tt1008@mail.tdc.edu.vn>
2023-10-27 23:51:52 +0700

    push user

---
+++ luxury/resources/views/user.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/67ff6e11b9.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col1">
+                    <i class="fa-solid fa-location-dot fa-xl"></i>
+                    <img src="images/zalo.png" alt="zalo" style="width: 18px; height: 18px;">
+                    <i class="fa-solid fa-phone-volume fa-xl">1900 111 999</i>
+                </div>
+                <div class="col2">
+                    <i class="fa-solid fa-cart-shopping fa-xl" style="float: right;">
+                        <h4">Giỏ hàng</h4>
+                    </i>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div class="titleLogo"><a href="">
+                            <img src="images/Logo.png" alt="" style="width: 150px; height: 170px;">
+                        </a></div>
+                </div>
+                <div class="col-9" style="padding-top: 50px;">
+                    <div class="title"><a href="">Trang Chủ </a> <span class="px-3"> > <span class="px-3"> Thông tin tài khoản</span></span></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
