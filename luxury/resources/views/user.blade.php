4c59cec396efd7a9261eb1091a45816588ae641b
Vietlam03 <21211tt1008@mail.tdc.edu.vn>
2023-11-02 22:49:16 +0700

    push user

--- luxury/resources/views/user.blade.php
+++ luxury/resources/views/user.blade.php
@@ -8,8 +8,7 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/67ff6e11b9.js" crossorigin="anonymous"></script>
 </head>
 
@@ -33,46 +32,62 @@
     <div class="main">
         <div class="container">
             <div class="row">
+                <!-- Logo -->
                 <div class="col-3">
                     <div class="titleLogo"><a href="">
                             <img src="images/Logo.png" alt="" style="width: 150px; height: 170px;">
-                    </a></div>
-
+                        </a>
+                    </div>
+                <!-- Menu User -->
                     <div class="menuUser">
                         <p><br></p>
                         <p><a href=""> Thông tin tài khoản </a></p>
-                        <p><a href=""> Mã giảm giá </a></p >
+                        <p><a href=""> Mã giảm giá </a></p>
                         <p><a href=""> Lịch sử mua hàng </a></p>
                         <p><img src="images/logout.png" alt=""><a href="" style="color:red"> Đăng xuất </a></p>
                     </div>
                 </div>
+                <!-- Indexing -->
                 <div class="col-9" style="padding-top: 50px;">
                     <div class="title"><a href="">Trang Chủ </a> <span class="px-3"> > <span class="px-3 fs-3"> Thông tin tài khoản</span></span></div>
-
+                <!-- infoUser basic -->
                     <div class="infoUser">
                         <h3 class="pt-4"><img src="images/user.png" alt=""> <span class="ps-4"> Nguyễn Văn A <span style="padding-left:80px">Mã khách hàng: 123456789</span></span></h3>
-                        <div class="row ps-3 pt-4">
-                        <div class="col info">
-                            <label for="">Username</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-user fa-xl ps-3"></i> <span class="ps-3 pt-2">nguyenvana03 </span></p>
-                            <label for="">Họ tên</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-user fa-xl ps-3"></i> <span class="ps-3 pt-2">NGUYỄN VĂN A </span></p>
-                            <label for="">Số điện thoại</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-phone-volume fa-lg ps-3"></i> <span class="ps-3 pt-2">0919 191 919 </span></p>
-                            <label for="">Địa chỉ</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-location-dot fa-xl ps-3"></i> <span class="ps-3 pt-2">12/5 Đường Top , Thành phố Thủ Đức </span></p>
-                        </div>
-                        <div class="col info">
-                            <label for="">Password</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-key fa-xl ps-3"></i> <span class="ps-3 pt-2">*********** </span></p>
-                            <label for="">Email</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-envelope fa-xl ps-3"></i> <span class="ps-3 pt-2">nguyenvana303@gmail.com </span></p>
-                            <label for="">Ngày sinh</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-calendar fa-xl ps-3"></i> <span class="ps-3 pt-2">01/01/2003 </span></p>
-                            <label for="">Giới tính</label>
-                            <p class="border border-dark rounded-pill"><i class="fa-solid fa-mars-and-venus fa-xl ps-3"></i> <span class="ps-3 pt-2">Nam </span></p>
-                        </div>
-                        </div>
+
+                        <!-- form info detail -->
+                        <form action="" method="post">
+                            <div class="row ps-3 pt-4">
+                                <div class="col info">
+                                    <label for="username">Username</label> <br>
+                                    <input type="text" name="username" size="30" maxlength="50" required style="background-image: url(/images/user.png);  background-size:35px 25px;background-position: left center 5px; background-repeat: no-repeat;padding-left:40px" class="border border-dark rounded-pill" value="nguyenvana03"> <br> <br>
+                                    <label for="fullname">Họ tên</label> <br>
+                                  
+                                    <input type="fullname" name="fullname" size="30" maxlength="50" required style="background-image: url(/images/user.png);  background-size:35px 25px;background-position: left center 5px; background-repeat: no-repeat;padding-left:40px" class="border border-dark rounded-pill" value="NGUYỄN VĂN A"><br> <br>
+                                    <label for="phone">Số điện thoại</label> <br>
+                                    <input type="tel" name="phone" size="30" pattern="\d{10}" required style="background-image: url(/images/phone.png); background-size:45px 25px; background-position: left center 3px; background-repeat: no-repeat;padding-left:40px" class="border border-dark rounded-pill" value="0919191919"> <br> <br>
+                                    <label for="address">Địa chỉ</label> <br>
+                                    <input type="text" name="address" size="30" style="background-image: url(/images/address.png); background-size:35px 20px; background-position: left 5px; background-repeat: no-repeat;padding-left:40px" class="border border-dark rounded-pill" value="12/5 Đường Top , Thành phố Thủ Đức">
+                                </div>
+
+                                <div class="col info">
+                                    <label for="password">Password</label> <br>
+                                    <input  type="password" name="password" size="30" maxlength="20" required style="background-image: url(/images/pass.png); background-size:25px 25px; background-position: left center 5px; background-repeat: no-repeat;padding-left:40px" class="border border-dark rounded-pill" value="avangnuyen03">  <br> <br>
+                                    <label for="email">Email</label> <br>
+                                    <input type="email" name="email" size="30" maxlength="50" required style="background-image: url(/images/email.png); background-size:20px 25px; background-position: left center 5px; background-repeat: no-repeat;padding-left:40px" class="border border-dark rounded-pill" value="nguyenvana303@gmail.com"> <br> <br>
+                                    <label for="DOB">Ngày sinh</label> <br>
+                                    <input type="date" name="DOB" style="width: 317px; height:28px"  class="border border-dark rounded-pill"> <br> <br>
+                                    <label for="gender">Giới tính</label> <br>
+                                    <select name="gender" style="width: 317px; height:28px"  class="border border-dark rounded-pill" value="nguyenvana03">
+                                        <option value="male" >Male</option>
+                                        <option value="female">Female</option>
+                                        <option value="female">Other</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <br>
+                            <input type="submit" name="submit" value="Sửa" style="margin-left: 334px; border-radius:20px;width:100px;border:solid gray 1px">
+                        </form>
+
                     </div>
                 </div>
             </div>
