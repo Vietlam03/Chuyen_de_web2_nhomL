feee813120eae812239f33576fd99a3de47ef6f8
Thang3160 <tranquangthang3160@gmail.com>
2023-11-06 17:22:57 +0700

    bg-img

--- luxury/resources/views/auth/forgot-password.blade.php
+++ luxury/resources/views/auth/forgot-password.blade.php
@@ -1,8 +1,11 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
 
+    <div class="mb-4 text-md text-black">
+        {{ __('Bạn quên mật khẩu? Không vấn đề. Chỉ cần cho chúng tôi biết địa chỉ email của bạn và chúng tôi sẽ gửi cho bạn liên kết đặt lại mật khẩu qua email để cho phép bạn chọn địa chỉ mới.') }}
+    </div>
+    <b class="text-center">
+        <p style="color: black;">Vui Lòng nhập Email dưới đây</p>
+    </b>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -16,9 +19,9 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-center mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Xác nhận') }}
             </x-primary-button>
         </div>
     </form>
