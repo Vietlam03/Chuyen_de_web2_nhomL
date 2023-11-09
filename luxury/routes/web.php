758e1560b508cf9660d42137174b9902d819bf17
DuyThee <21211tt1988@mail.tdc.edu.vn>
2023-11-09 07:40:14 +0700

    push chuc nang them san pham

--- luxury/routes/web.php
+++ luxury/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/create', function () {
+    return view('products.create');
+});
+
+Route::get('/index', function () {
+    return view('products.index');
+});
+
+Route::resource('categories', CategoryController::class);
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
