eb3babffbbd8da8e279ca8f8d39905d1d6bab8a4
Thang3160 <tranquangthang3160@gmail.com>
2023-11-09 09:39:38 +0700

    coment route web

--- luxury/routes/web.php
+++ luxury/routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//man hinh trang chu
 Route::get('/', function () {
     return view('welcome');
 });
-
+// man hinh trang usser
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
